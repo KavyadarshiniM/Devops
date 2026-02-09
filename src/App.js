1d1c5d8e0b6f1a07c31b13a53bb9f86874b34f52
KavyadarshiniM <kavyadarshinim2005@gmail.com>
2026-02-09 15:10:58 +0530

    Third commit

--- src/App.js
+++ src/App.js
@@ -267,7 +267,7 @@ function ProgramsPage() {
     <div className="page programs-page">
       <section className="programs-hero">
         <div className="programs-hero-content">
-          <h1>Acdemic Programs</h1>
+          <h1>Acaaademic Programs</h1>
           <p>Choose from our diverse range of engineering programs and skill-based certifications</p>
         </div>
       </section>
